59413555574959ab6080185d520024538bd0b484
James Stradling <james@strdlng.com>
2022-04-13 00:06:13 +1300

    Update Package.swift to specify Pitchy as a target dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Beethoven-iOS",
-            dependencies: [],
+            dependencies: ["Pitchy"],
             path: "Source"),
         .testTarget(
             name: "Beethoven-iOS-Tests",
