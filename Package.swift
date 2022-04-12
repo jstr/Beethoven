84ebaf9e74d62ba33c6665f86a79b6873d4537df
James Stradling <james@strdlng.com>
2022-04-12 23:18:26 +1300

    Update Package.swift to require Pitchy fork with that works with recent SPM

--- Package.swift
+++ Package.swift
@@ -1,8 +1,29 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Beethoven",
-  dependencies: [
-    .Package(url: "https://github.com/vadymmarkov/Pitchy.git", majorVersion: 2),
-  ]
+    name: "Beethoven",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Beethoven",
+            targets: ["Beethoven-iOS"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/jstr/Pitchy", .branch("update-spm")),
+
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Beethoven-iOS",
+            dependencies: []),
+        .testTarget(
+            name: "Beethoven-iOS-Tests",
+            dependencies: ["Beethoven-iOS"]),
+    ]
 )
